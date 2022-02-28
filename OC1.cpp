79648eaa20701381f42137f21c582d3eabfc824e
dan <danilsocol-2002@mail.ru>
2022-02-28 23:37:46 +0500

    Final

--- OC1.cpp
+++ OC1.cpp
@@ -4,6 +4,8 @@
 #include <conio.h>
 #include <stdlib.h> 
 #include "OC1.h"
+#include <string>
+#include <string>
 
 using namespace std;
 
@@ -14,63 +16,135 @@ typedef struct _Anketa
 	int age;
 } Anketa;
 
-void Viewing()
-{
-	Anketa anketa;
-	DWORD bytes;
-	HANDLE hFile;
-	hFile = CreateFile(L"Text.dat", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	SetFilePointer(hFile, sizeof(Anketa), 0, FILE_BEGIN);
-	ReadFile(hFile, &anketa, sizeof(Anketa), &bytes, NULL);
-	CloseHandle(hFile);
-	printf("%d \n", anketa.age);
+HANDLE openFile(LPCWSTR nameOfFile) {
+	HANDLE file = CreateFile(
+		nameOfFile,
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+	return file;
 }
 
-void Editing()
-{
-
-
-
+DWORD writeFile(HANDLE file, char* data, int size) {
+	DWORD bytesWritten;
+	WriteFile(
+		file,
+		data,
+		size,
+		&bytesWritten,
+		NULL);
+	return bytesWritten;
 }
 
-void AddNew()
-{
-	DWORD bytes;
-	HANDLE hFile;
-	hFile = CreateFile(L"Text.dat", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+Anketa readFile(HANDLE file, int size) {
+	Anketa data;
+	DWORD bytesWritten;
+	ReadFile(file,
+		&data,
+		size,
+		&bytesWritten,
+		NULL);
+	return 	data;
+}
 
-	cout << "Создайте сво анкету" << endl;
+void lockFile(HANDLE file, int start, int size, int access) {
+	OVERLAPPED ol;
+	ol.OffsetHigh = 0;
+	ol.Offset = start;
+	ol.hEvent = NULL;
+	LockFileEx(file, access, 0, size, 0, &ol);
+}
+
+void writeProfileInFileAdd(HANDLE file, Anketa profile) {
+	SetFilePointer(file, 0, 0, FILE_END);
+	writeFile(file, (char*)&profile, sizeof(profile));
+}
+
+void writeProfileInFileEditing(HANDLE file, Anketa profile,int num) {
+	SetFilePointer(file, sizeof(Anketa) * (num - 1), 0, FILE_BEGIN);
+	writeFile(file, (char*)&profile, sizeof(profile));
+}
+
+Anketa readProfile(HANDLE file, int numOfResume) {
+	int startPoint = sizeof(Anketa) * (numOfResume - 1);
+	SetFilePointer(file, sizeof(Anketa) * (numOfResume - 1), 0, FILE_BEGIN);
+	lockFile(file, startPoint, sizeof(Anketa), 0);
+	return readFile(file, sizeof(Anketa));
+}
+
+void writeProfile(Anketa profile) {
+	printf("%d\n", profile.age);
+	printf("%s\n", profile.name);
+}
+
+void AddNew(HANDLE file)
+{
+
+	cout << "Create your profile" << endl;
 	Anketa newAnketa;
-	cout << "Введите свое имя" << endl;
+	cout << "Enter your name" << endl;
 	cin >> newAnketa.name;
-	cout << "Введите свой возраст" << endl;
+	cout << "Enter your age" << endl;
 	cin >> newAnketa.age;
 
-	WriteFile(hFile, &newAnketa, sizeof(Anketa), &bytes, NULL);
-	CloseHandle(hFile);
-
+	writeProfileInFileAdd(file, newAnketa);
 }
 
+void Viewing(HANDLE file)
+{
+	int num;
+	cout << "Select the questionnaire number" << endl;
+	cin >> num;
+
+	Anketa profile = readProfile(file, num);
+	writeProfile(profile);
+}
+
+void Editing(HANDLE file)
+{
+	int num;
+	cout << "Select the questionnaire number to edit" << endl;
+	cin >> num;
+
+	int startPoint = sizeof(Anketa) * (num - 1);
+	lockFile(file, startPoint, sizeof(Anketa), LOCKFILE_EXCLUSIVE_LOCK);
+
+	cout << "Editing your profile" << endl;
+	Anketa newAnketa;
+	cout << "Enter your name" << endl;
+	cin >> newAnketa.name;
+	cout << "Enter your age" << endl;
+	cin >> newAnketa.age;
+
+	SetFilePointer(file, startPoint, 0, FILE_BEGIN);
+	writeFile(file, (char*)&newAnketa, sizeof(newAnketa));
+}
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 
 	while (true)
 	{
-		/*HANDLE hFile;
-		hFile = CreateFile(L"Text.dat", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);*/
-
+		HANDLE file;
+		// hFile = CreateFile(L"Text.dat", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+		LPCWSTR path = L"C:\\Users\\Seraphic\\Desktop\\test.bin";
 		char num;
-		cout << "Выберете действие 1 - Просмотр анкеты, 2 - Редактирование анкеты, 3 - Добавление новой анкеты" << endl;
+		file = openFile(path);
+
+		cout << "Select action 1 - Viewing the questionnaire, 2 - Editing the questionnaire, 3 - Adding a new questionnaire" << endl;
 		cin >> num;
 		if (num == '1')
-			Viewing();
+			Viewing(file);
 		else if (num == '2')
-			Editing();
+			Editing(file);
 		else if (num == '3')
-			AddNew();
+			AddNew(file);
+
+		CloseHandle(file);
 	}
 
 
---
+++ OC1.h
@@ -0,0 +1,1 @@
+static HANDLE openFile(char nameOfFile);
